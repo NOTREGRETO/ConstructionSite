d75fbdfc4bcf1ee367c7d3c9a9dcaef0fd596c0b
samarth3301 <samarplayz1337@gmail.com>
2026-04-06 09:51:26 +0530

    Add Project type definitions for Sanity integration

---
+++ src/types/project.ts
@@ -0,0 +1,26 @@
+import { SanityImageAssetDocument } from 'next-sanity'
+
+export interface Project {
+  _id: string
+  _createdAt: string
+  _updatedAt: string
+  title: string
+  slug: {
+    current: string
+  }
+  label?: string
+  location?: string
+  category?: string
+  description?: string
+  image?: {
+    asset: SanityImageAssetDocument
+    hotspot?: {
+      x: number
+      y: number
+      height: number
+      width: number
+    }
+  }
+  featured: boolean
+  order?: number
+}
